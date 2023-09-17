85984b984de7d17c79d4ed491599044cac59080c
athena272 <guilhermera272@gmail.com>
2023-09-17 09:35:32 -0300

    Update App.jsx

--- 18_missao-react/code-01/src/App.jsx
+++ 18_missao-react/code-01/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
