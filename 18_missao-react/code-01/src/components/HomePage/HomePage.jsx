406e4653da8650db8f2cda5fffa1e4612788ec23
athena272 <guilhermera272@gmail.com>
2023-09-17 09:35:53 -0300

    Create HomePage.jsx

---
+++ 18_missao-react/code-01/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../Card/Card";
+
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="container">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    );
+}
+
+export default HomePage;
