c992a804a542aa59c055571c03c8992022b64584
athena272 <guilhermera272@gmail.com>
2023-09-17 09:35:39 -0300

    Create Card.jsx

---
+++ 18_missao-react/code-01/src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Card.css'
+
+function Card() {
+    return (
+        <div className="card">
+            <div className="card-container">
+                <p>Componentes:</p>
+                <p>Facilita em manter partes menores funcionando corretamente</p>
+                <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
