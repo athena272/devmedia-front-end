e12ad3c62324caf1b5477325c07bac447eff7b19
athena272 <guilhermera272@gmail.com>
2023-09-17 04:54:12 -0300

    add react firtst code

---
+++ 17_missao-react/code-01/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <p>Com React JS</p>
+      <p className="autor">Criado por Mateus Correa</p>
+    </div>
+  );
+}
+
+export default App;
