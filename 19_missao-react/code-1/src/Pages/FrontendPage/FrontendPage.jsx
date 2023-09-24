ffa2b78a2e8fd37a6bee7524c83c2e377e9bdb56
athena272 <guilhermera272@gmail.com>
2023-09-24 08:58:03 -0300

    Update FrontendPage.jsx

--- 19_missao-react/code-1/src/Pages/FrontendPage/FrontendPage.jsx
+++ 19_missao-react/code-1/src/Pages/FrontendPage/FrontendPage.jsx
@@ -9,7 +9,7 @@ const FrontendPage = () => (
             React é uma biblioteca JavaScript para a criação de interfaces de usuário e, nesse contexto, uma das tecnologias mais utilizadas pelo mercado. No guia abaixo você aprenderá quais são os pré-requisitos para iniciar no React do jeito certo e como criar as suas primeiras SPAs com ela.
         </p>
 
-        <a href="https://www.devmedia.com.br/react/"> Guia de React</a>
+        <a href="https://www.devmedia.com.br/react/" target='_blank' rel="noreferrer"> Guia de React</a>
 
         <ReturnButton />
     </div>
