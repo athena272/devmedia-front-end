7e498bf49835c0a282280aa25ff328f427f5571e
athena272 <guilhermera272@gmail.com>
2023-09-24 08:58:01 -0300

    Update BackendPage.jsx

--- 19_missao-react/code-1/src/Pages/BackendPage/BackendPage.jsx
+++ 19_missao-react/code-1/src/Pages/BackendPage/BackendPage.jsx
@@ -9,7 +9,7 @@ const BackendPage = () => (
             Node.js é uma tecnologia usada para executar código JavaScript fora do navegador. Com ele podemos construir aplicações web em geral, desde web sites até APIs e microsserviços.
         </p>
 
-        <a href="https://www.devmedia.com.br/node-js/"> Guia de Node.js</a>
+        <a href="https://www.devmedia.com.br/node-js/" target='_blank' rel="noreferrer"> Guia de Node.js</a>
 
         <ReturnButton />
     </div>
