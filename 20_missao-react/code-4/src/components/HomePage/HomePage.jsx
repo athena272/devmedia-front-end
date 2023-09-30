d0fa21ac9b265f0f63164cda0f52227d2e822a2a
athena272 <guilhermera272@gmail.com>
2023-09-30 15:33:08 -0300

    Create HomePage.jsx

---
+++ 20_missao-react/code-4/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import './HomePage.css';
+
+const HomePage = () => {
+    const [randomNumber, setRandomNumber] = useState(0);
+
+    function generateNumber() {
+        const newNumber = Math.floor(Math.random() * 101);
+        setRandomNumber(newNumber);
+    }
+
+    return (
+        <div className="conteudo-centralizado">
+            <h3>Número aleatório:</h3>
+            <h1>{randomNumber}</h1>
+
+            <div className='area-botao'>
+                <label>
+                    Click no botão abaixo para gerar um número aleatório:
+                </label>
+
+                <button onClick={generateNumber}>
+                    Gerar número
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
