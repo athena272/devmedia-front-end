ec1dc9637ba200d6b2a51b65fff562ed1bae80d5
athena272 <guilhermera272@gmail.com>
2023-10-01 13:26:07 -0300

    Create Footer.jsx

---
+++ 21_missao-react/code-02/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => (
+    <footer className="rodape">
+        <div className="limitar-container">
+            © Copyright 2021   |   Consultório dentes saudáveis
+        </div>
+    </footer>
+)
+
+export default Footer;
