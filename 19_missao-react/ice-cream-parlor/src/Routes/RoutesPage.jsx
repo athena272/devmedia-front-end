2065ad4de076e141053d0783edf74fc54d6978ca
athena272 <guilhermera272@gmail.com>
2023-10-22 17:16:16 -0300

    Update RoutesPage.jsx

--- 19_missao-react/ice-cream-parlor/src/Routes/RoutesPage.jsx
+++ 19_missao-react/ice-cream-parlor/src/Routes/RoutesPage.jsx
@@ -7,7 +7,7 @@ import AboutPage from '../Pages/AboutPage/AboutPage';
 const RoutesPage = () => (
     <BrowserRouter> {/* Componente que armazena as rotas */}
         <Routes> {/* Seleciona apenas um componente para exibir na tela baseado na url */}
-            <Route path='/' element={<HomePage />} /> {/* Vomponente que associa a rota ao componente */}
+            <Route path='/' element={<HomePage />} /> {/* Componente que associa a rota ao componente */}
             <Route path='/flavors' element={<FlavorsPage />} />
             <Route path='/about' element={<AboutPage />} />
         </Routes>
