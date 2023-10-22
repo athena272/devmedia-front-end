a2afd489d1277ed6032063e2a465cd3bdea4e525
athena272 <guilhermera272@gmail.com>
2023-10-22 15:29:50 -0300

    Update App.jsx

--- 21_missao-react/code-03/src/App.jsx
+++ 21_missao-react/code-03/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import AnimalCard from './components/AnimalCard/AnimalCard'
+import InfoCard from './components/InfoCard/InfoCard'
+import Header from './components/Header/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [componentType, setComponentType] = useState('cachorro')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    console.log(componentType)
+
+    return (
+        <div className="App">
+            <Header animalType={componentType} setState={setComponentType} />
+            <AnimalCard animalType={componentType} />
+            <InfoCard animalType={componentType} />
+        </div>
+    );
 }
 
-export default App
+export default App;
