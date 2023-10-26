1f134481b85f1fcc8e3e436fc24dec509177b50c
athena272 <guilhermera272@gmail.com>
2023-10-26 09:51:04 -0300

    Update helper.js

--- helper.js
+++ helper.js
@@ -14,3 +14,19 @@ const programadores = [
 const buscarProgramadoresRuby = () => programadores.filter(programador => programador.linguagem === 'Ruby')
 
 console.log(buscarProgramadoresRuby());
+
+const carros = [
+    { marca: 'Fiat', modelo: 'Uno', anoFabricacao: 2015 },
+    { marca: 'GM', modelo: 'Onix', anoFabricacao: 2023 },
+    { marca: 'Ford', modelo: 'KA+', anoFabricacao: 2023 },
+    { marca: 'Fiat', modelo: 'Cronos', anoFabricacao: 2020 },
+];
+
+const carrosPorAno = () => {
+    let hoje = new Date();
+    let anoAtual = hoje.getFullYear();
+    const carrosDoAno = carros.filter(carro => carro.anoFabricacao === anoAtual);
+    return carrosDoAno
+}
+
+console.log(carrosPorAno())
