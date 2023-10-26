13b7344dac151bfe7b3287bb3df507953a767c6b
athena272 <guilhermera272@gmail.com>
2023-10-25 22:21:03 -0300

    Update App.jsx

--- 22_missao-react/code-02/src/App.jsx
+++ 22_missao-react/code-02/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { produtosFiltrados } from './service/service.js'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <div>
+    <h2>Lista de produtos para comprar</h2>
+    <ul>
+      {produtosFiltrados.map((produto) => (
+        <li key={produto.id}>
+          {produto.nome} - R$ {produto.preco.toFixed(2)}
+        </li>
+      ))}
+    </ul>
+    <p>
+      Total: R${" "}
+      {produtosFiltrados
+        .reduce((total, produto) => total + produto.preco, 0)
+        .toFixed(2)}
+    </p>
+    <p>
+      {produtosFiltrados.length > 0
+        ? `Faltam comprar ${produtosFiltrados.length} produtos.` : "Você já comprou todos os produtos."}
+    </p>
+  </div>
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+export default App;
