35481659e47cc210894860cdc7ecb67ddc4e9f77
athena272 <guilhermera272@gmail.com>
2023-10-25 22:21:00 -0300

    Create data.js

---
+++ 22_missao-react/code-02/src/data/data.js
@@ -0,0 +1,9 @@
+const produtos = [
+    { id: 1, nome: "Arroz", preco: 5.99, comprado: true },
+    { id: 2, nome: "Feijão", preco: 8.49, comprado: true },
+    { id: 3, nome: "Macarrão", preco: 4.29, comprado: true },
+    { id: 4, nome: "Carne", preco: 25.9, comprado: false },
+    { id: 5, nome: "Leite", preco: 3.99, comprado: false }
+];
+
+export default produtos;
