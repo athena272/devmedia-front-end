df7abb985f71ac86215ea32cba5d7fb7d7bd2e81
athena272 <guilhermera272@gmail.com>
2023-12-15 22:25:54 -0300

    Create helloWorld.ts

---
+++ 23_Typescript/code-02/helloWorld.ts
@@ -0,0 +1,8 @@
+//variável mensagem com tipo definido (string)
+
+let mensagem: string = "Hello World";
+
+//console.log recebendo como parâmetro a variável 'mensagem',
+//para exibir o texto associado a ela no console do navegador.
+
+console.log(mensagem);
